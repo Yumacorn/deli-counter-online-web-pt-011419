375efc33ff52bd1a25f58a2945f3351b6bd86ff0
Ryan Mui <reachryanmui@gmail.com>
2019-01-29 02:44:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(array)
   lineup = ""
-  array.each_with_index {|x, index| lineup << index+1 +". " x}
+  array.each_with_index {|x, index| lineup << index+1 + ". "  +x}
   if lineup == ""
     puts "The line is currently empty."
   else
